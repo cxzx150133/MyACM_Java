42cf8a2342f62ebe3b8d7007e64363ed35b9a0a2
GPLer <GPLer@qq.com>
2020-10-13 08:57:36 +0800

    Add HDOJ Template.

---
+++ HDU/Template/Main.java
@@ -0,0 +1,21 @@
+package HDU.Template;
+
+//
+import java.lang.*;
+import java.io.*;
+import java.nio.*;
+import java.math.*;
+import java.util.*;
+import java.text.*;
+import java.net.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String  ln = System.lineSeparator();
+
+        //
+
+        in.close();
+    }
+}
---
+++ HDU/Template/ReadMe.md
@@ -0,0 +1,38 @@
+# 
+
+|Limit|Value|
+|---|---|
+|Time Limit: | MS (Java/Others)|
+|Memory Limit: | K (Java/Others)|
+
+## Problem Description
+
+
+
+## Input
+
+
+
+## Output
+
+
+
+## Sample Input
+
+```
+
+```
+
+## Sample Output
+
+```
+
+```
+
+## Author
+
+
+
+## Recommend
+
+
