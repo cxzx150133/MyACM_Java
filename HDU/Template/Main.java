bfeac7371b27f226fa7ed0c4a0b59034b235edf0
GPLer <GPLer@qq.com>
2020-10-13 11:34:23 +0800

    update Template

--- HDU/Template/Main.java
+++ HDU/Template/Main.java
@@ -12,9 +12,16 @@ import java.net.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        PrintStream out = System.out;
+
+        PrintWriter cout = new PrintWriter(new OutputStreamWriter(System.out));
+        BufferedInputStream cin = new BufferedInputStream(System.in);
+
         String  ln = System.lineSeparator();
 
-        //
+        while (in.hasNext()) {
+            int N = Integer.parseInt(in.next());
+        }
 
         in.close();
     }
